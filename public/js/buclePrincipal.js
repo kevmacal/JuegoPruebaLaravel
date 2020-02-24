67317e93d10da6b0a4d3d2d4bca73ff2ae7cd606
MarlonDevelop <kevmacal@espol.edu.ec>
2020-02-24 11:16:35 -0500

    Prueba de Bucle Principal fps y aps

---
+++ public/js/buclePrincipal.js
@@ -0,0 +1,31 @@
+/*namespace - espacio de nombres
+mainloop - bucleprincipal
+aps - actualizaciones por segundo
+fps - frames por segundo*/
+
+var buclePrincipal = {
+    idEjecucion: null,
+    ultimoReg: 0,
+    aps: 0,
+    fps: 0,
+    iterar: function(registroTemporal){
+        buclePrincipal.idEjecucion = window.requestAnimationFrame(buclePrincipal.iterar);
+        buclePrincipal.actualizar(registroTemporal);
+        buclePrincipal.dibujar();
+
+        if(registroTemporal-buclePrincipal.ultimoReg>999){
+            buclePrincipal.ultimoReg=registroTemporal;
+            console.log("APS: "+buclePrincipal.aps+" FPS: "+buclePrincipal.fps);            
+            buclePrincipal.aps = 0;
+            buclePrincipal.fps = 0;
+        }
+    },
+    detener: function(){
+    },
+    actualizar: function(registroTemporal){
+        buclePrincipal.aps=buclePrincipal.aps+1;        
+    },
+    dibujar: function(registroTemporal){
+        buclePrincipal.fps=buclePrincipal.fps+1;
+    },
+};
--- public/js/prueba.js
+++ public/js/prueba.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
-    console.log("Funciona Javascript!!!");
+    //console.log("Funciona Javascript!!!");
+    prueba.iniciarJuego();
 },false);
 /* DOM = DOCUMENT OBJECT MODEL */
+
+var prueba ={
+    iniciarJuego: function(){
+        console.log("¡Juego Iniciado!");
+        buclePrincipal.iterar();
+    },
+};
