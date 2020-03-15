607c3488208dd41a9566f41447f470b3d095bcb4
MarlonDevelop <kevmacal@espol.edu.ec>
2020-03-15 01:13:40 -0500

    Correccion mapa, avance paletaSprite

---
+++ public/js/paletaSprite.js
@@ -0,0 +1,27 @@
+function paletaSprite(dataSprite){
+    this.rutaImagen = dataSprite.image;
+    this.widthImage = parseInt(dataSprite.imagewidth);
+    this.heightImage = parseInt(dataSprite.imageheight);
+
+    this.widthSprite = parseInt(dataSprite.tilewidth);
+    this.heightSprite = parseInt(dataSprite.tileheight);
+
+    this.firstSprite = parseInt(dataSprite.firstgid);
+    this.anchoImagenInSprite =  this.widthImage/this.widthSprite;
+    this.altoImagenInSprite =  this.heightImage/this.heightSprite;
+
+    this.totalSprites = thsi.anchoImagenInSprite*this.altoImagenInSprite;
+    this.sprites = [];
+
+    for(s=0;this.totalSprites;s++){
+        var idActual=this.firstSprite-1+s;
+        this.sprites.push(new sprite(this.rutaImagen, idActual));
+    }
+}
+
+paletaSprite.prototype.obtenerPosicionDesdeIdSprite = function(idSprite){
+    var y = Math.floor(idSprite / this.anchoImagenInSprite);
+    var x = idSprite % this.anchoImagenInSprite;
+
+    return new punto(x*this.widthSprite, y.this.heightSprite);
+}
